b9dddf8acd08b0afd65e9507422b0b942e2b49cb
shunak <shuna966@gmail.com>
2021-10-26 23:38:19 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate art;
+
+use art::kinds::PrimaryColor;
+use art::utils::mix;
+
 fn main() {
-    println!("Hello, world!");
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red,yellow);
 }
